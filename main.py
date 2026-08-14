8290903911acef5a45b1a5f3995e40a73d07f4be
guy-trying-network <isiomaobuh1@gmail.com>
2026-08-13 21:35:17 -0600

    4) Added forward moment to player and grouped players into an updatable and drawable container class

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,8 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         dt = clock.tick(60) / 1000
         pygame.display.flip()
 
